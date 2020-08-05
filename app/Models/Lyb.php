2fb9fbdbab2bd1fe7c81d5d1fe9bd4e5dc27a8e5
xfppp <xinyaaomen250qq.co@qq.com>
2020-08-05 20:28:28 +0800

    yamen

---
+++ app/Models/Lyb.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lyb extends Model
+{
+    //
+    protected $table = 'shop_lyb';
+    protected $primarykey = 'shop_lyb_id';
+    public $timestamps = false;
+}
